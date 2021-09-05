20e92620ee9a7840c59451a01dd011c7fcc9beaf
niknovikov19 <nikknovikov@gmail.com>
2021-09-05 18:47:59 +0300

    Fix and test ROI calculation for xarray DataArray
    Fix bugs in the function data_proc_common.calc_xarray_ROIs() so it
    works now correctly in the main scenarios.
    
    Write a test script for this function: test_calc_xarray_ROIs.py
    Test:
    - Create a 3-d DataArray that contains in a cell with the coordinates
      (i,j,k) a number equal to (100*k + 10*j + i), in the string format
    - One of the dimensions is associated with an additional coordinate with
      the values differing from the main values of this dimensions
    - Define several sets of ROI's - with one, two, or three dimentions covered
      by the ROI's from a set. Also try to set the ROI limits over the
      'additional' coordinate
    - Combine the DataArray content into ROI's using string concatenations as
      the reducing functions
    - Print the result and visually control its validity

--- data_proc_common.py
+++ data_proc_common.py
@@ -1,85 +1,113 @@
 # -*- coding: utf-8 -*-
-"""Provides common functions for processing various types of data
+"""Provides common functions for processing various types of data.
 
 """
 
 
-import os
+#import os
 
+import numpy as np
 #import pandas as pd
 import xarray as xr
 
-import useful as usf
+#import useful as usf
 
 
-def get_xarrray_dim_by_coord(X, c):
-    return X.coords[c].dims[0]
+def get_xarrray_dim_by_coord(X, coord):
+    return X.coords[coord].dims[0]
 
-def xarray_coords_to_dict(coords):
-    """Convert xarray coords into a dict, as used in Dataset constructor
-    
-    """
+def xarray_coords_to_dict(X, coords):
+    """Convert coords into a dict, as used in Dataset constructor."""
     d = dict()
-    for coord_name in coords.keys():
-        dim_name = get_xarrray_dim_by_coord(coord_name)
+    for coord_name, coord_vals in coords.items():
+        dim_name = get_xarrray_dim_by_coord(X, coord_name)
         if coord_name == dim_name:
-            d[coord_name] = coords[coord_name].values
+            d[coord_name] = coord_vals
         else:
-            d[coord_name] = (dim_name, coords[coord_name].values)
+            d[coord_name] = (dim_name, coord_vals)
     return d
 
 def generate_ROI_names(ROI_coords, ROI_descs):
     """Generate ROI names from coord names and limits
     
-    Warning: ROI name generation works fine only for integer coordinates
+    Warning: ROI name generation works fine only for integer coordinates.
     """
     ROI_names, ROI_names2 = [], []
     for ROI in ROI_descs:
         ROI_name_2 = 'ROI'
         for coord in ROI_coords:
-            ROI_name_2 += f'_({coord}={ROI[coord][0]}-{ROI[coord][1]})'
+            coord_val_first = ROI['limits'][coord][0]
+            coord_val_last = ROI['limits'][coord][1]
+            ROI_name_2 += f'_({coord}={coord_val_first}-{coord_val_last})'
         ROI_names.append(ROI['name'])
         ROI_names2.append(ROI_name_2)
     return ROI_names, ROI_names2
     
 
 def calc_xarray_ROIs(X_in, ROIset_dim_name, ROI_coords, ROI_descs,
-                     reduce_fun = (lambda X,dims: X.mean(dim=dims))):
-    """Groups xarray::DataArray content into ROIs over given dimensions
+                     reduce_fun=(lambda X, dims: X.mean(dim=dims))):
+    """Groups xarray::DataArray content into ROIs over given dimensions.
     
     ROI_coords = ('coord1', 'coord2', ...)
     
     ROI_descs = [
-        {'name': 'ROIname1', 'coord1': (x1first, x1last), 'coord2': (x2first, x2last), ...} 
+        {
+            'name': 'ROIname1',
+            'limits': {
+                'coord1': (x1first, x1last),
+                'coord2': (x2first, x2last),
+                ...
+            }
+        }
         ...
-        ]
+    ]
     
     """
 
     # Dimensions over which the ROIs are grouped
     ROI_dims = [get_xarrray_dim_by_coord(X_in, coord) for coord in ROI_coords]
     
-    # Initialize output dims and coords
-    dims_out = list(X_in.dims.keys()).copy()
-    coords_out = xarray_coords_to_dict(X_in.coords)
+    # Select dimensions not covered by ROI
+    # dims_out - names of the selected dimension
+    # dim_nums_out - positions of the selected dimensions in the list
+    # of all input dimensions
+    # shape_out - shape list without elements corresponding to ROI dimensions
+    dim_data_out = [(dim_num, dim_name)
+                for dim_num, dim_name in enumerate(X_in.dims)
+                if dim_name not in ROI_dims]
+    if dim_data_out:
+        dim_nums_out, dims_out = zip(*dim_data_out)
+    else:
+        dim_nums_out, dims_out = [], []
+    shape_out = [X_in.shape[n] for n in dim_nums_out]
     
-    # Exclude output dims and coords that are collapsed into ROIs
-    for m, coord in enumerate(ROI_coords):
-        usf.list_remove(dims_out, ROI_dims[m])
-        usf.dict_remove(coords_out, coord)
-        
-    # Add output ROI-related dim and coords (num, name, name2)
+    # Select coordinates not covered by ROI
+    coords_out = {coord_name: coord_val.values
+                  for coord_name, coord_val in X_in.coords.items()
+                  if get_xarrray_dim_by_coord(X_in, coord_name) not in ROI_dims}
+    coords_out = xarray_coords_to_dict(X_in, coords_out)
+
+    # Add a new first dimension that corresponds to ROI numbers
     dim_new = ROIset_dim_name + '_num'
+    dims_out = list(dims_out)
     dims_out.insert(0, dim_new)
+    shape_out = [len(ROI_descs)] + shape_out
+    
+    # Add new coordinates corresponding to the new 'ROI-number' dimension
+    # 1. Contains ROI numbers (0, 1, 2, ...)
+    # 2. Contains ROI names given from the outside
+    # 3. Contains ROI names that are generated from the coordinate names
+    # and limits that define a ROI
     ROI_names, ROI_names2 = generate_ROI_names(ROI_coords, ROI_descs)
     coords_new = dict()
-    coords_new[dim_new] = range(len(ROI_descs))
+    coords_new[dim_new] = np.arange(len(ROI_descs))
     coords_new[ROIset_dim_name + '_name']  = (dim_new, ROI_names)
     coords_new[ROIset_dim_name + '_name2'] = (dim_new, ROI_names2)
     coords_out = {**coords_new, **coords_out}
     
     # Create output xarray
-    X_out = xr.DataArray(coords=coords_out, dims=dims_out)
+    x_out = np.ndarray(shape=shape_out, dtype=X_in.dtype)
+    X_out = xr.DataArray(x_out, coords=coords_out, dims=dims_out)
     
     # Calculate ROI data
     for n, ROI in enumerate(ROI_descs):
@@ -88,11 +116,13 @@ def calc_xarray_ROIs(X_in, ROIset_dim_name, ROI_coords, ROI_descs,
         index = dict()
         for m, coord in enumerate(ROI_coords):
             dim = get_xarrray_dim_by_coord(X_in, coord)
-            index[dim] = ((X_in.coords[coord] >= ROI_descs[coord][0]) &
-                          (X_in.coords[coord] <= ROI_descs[coord][1]))
+            coord_vals = X_in.coords[coord].values
+            index[dim] = (
+                (coord_vals >= ROI_descs[n]['limits'][coord][0]) &
+                (coord_vals <= ROI_descs[n]['limits'][coord][1]))
             
         # Select input slice by the index, process it, and store to the output
-        X_out[dict(dim_new=n)] = reduce_fun(X_in[index], ROI_dims)
+        X_out[{dim_new: n}] = reduce_fun(X_in[index], ROI_dims)
         
     # Copy attributes and add info about the operation
     X_out.attrs = X_in.attrs.copy()
---
+++ test_calc_xarray_ROIs.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""Tests for ROI reducing functions.
+
+"""
+
+import itertools
+
+import numpy as np
+import xarray as xr
+
+#import useful as usf
+import data_proc_common as dpc
+
+
+# Reducing functions used for testing data_proc_common::calc_xarray_ROIs()
+
+def reduce_fun_1d_strjoin(x):
+    """Join all elements of a string numpy 1-d array.
+    
+    This function is used to test reducing of n-d data over ROI's. 
+    In this case, the 'reduction' over a ROI is just a concatenation of
+    the elements that belong to this ROI.
+    """
+    return  '_'.join(x)
+
+def reduce_fun_np(x: np.ndarray, axes_reduce, reduce_fun_1d):
+    """Reduce a numpy ndarray over an unrolled combination of axes."""
+    
+    # Rearrage axes so the axes to reduce are the last ones
+    axes_orig = np.arange(x.ndim)
+    axes_new = np.concatenate((np.delete(axes_orig, axes_reduce), axes_reduce))
+    y = np.moveaxis(x, axes_new, axes_orig)
+    
+    # Unroll to-be-reduced axes
+    ndims_reduce = len(axes_reduce)
+    ndims_keep = y.ndim - ndims_reduce
+    shape_keep = np.array(y.shape)[:ndims_keep]
+    shape_new = np.append(shape_keep, -1)
+    y = y.reshape(shape_new)
+    
+    # Allocate output
+    z = np.ndarray(shape_keep, dtype=x.dtype)
+    
+    # Walk trough the combinations of non-reduced coordinates and
+    # apply a reduce function
+    coords_keep = [np.arange(l) for l in shape_keep]
+    len_reduce = y.shape[-1]
+    idx = itertools.product(*coords_keep)
+    for id in idx:
+        ys = y[(*id,range(len_reduce))]    # to-be-reduced 1-d slice
+        z[id] = reduce_fun_1d(ys)
+        
+    return z
+
+def reduce_fun_xarray(X: xr.DataArray, dims_reduce, reduce_fun_1d):
+    """Reduce an xr DataArray over an unrolled combination of dims."""
+    reduce_fun = (lambda X, axis: reduce_fun_np(X, axis, reduce_fun_1d))
+    return X.reduce(reduce_fun, dims_reduce)
+
+
+# Create test 3-d DataArray's that contains in a cell with the
+# coordinates (i,j,k) a number equal to (100*k + 10*j + i) or
+# its string represresentation
+
+def lst_encode(lst, k=1):
+    """[i, j, k] -> (100k + 10*j + i) """
+    res = 0
+    for n, x in enumerate(lst):
+        res += (x * (10**k)**n)
+    return res
+
+dims = ['dim0', 'dim1', 'dim2']
+sz = [3, 2, 4]
+coords = {
+        'dim0':     np.arange(sz[0]),    # 0, 1, 2
+        'dim1':     np.arange(sz[1]),    # 0, 1
+        'dim2':     np.arange(sz[2]),    # 0, 1, 2, 3
+        'dim2a':    ('dim2', 10*np.arange(sz[2]))
+        }
+
+x = np.ndarray(sz, dtype=np.int)
+dim_coords = [coord_val for coord_name, coord_val in coords.items()
+              if coord_name in dims]
+idx = itertools.product(*dim_coords)
+for id in idx:
+    x[id] = lst_encode(list(id))
+    
+xs = x.astype(np.str)
+xs = xs.astype(np.object)
+
+X = xr.DataArray(x, coords=coords, dims=dims)    
+Xs = xr.DataArray(xs, coords=coords, dims=dims)
+
+
+ROI_coords = {}
+ROI_descs = {}
+
+# Test 1: reduce (dim0, dim2)
+ROI_coords[0] = ['dim0', 'dim2a']
+ROI_descs[0] = [
+        {'name': 'ROI0', 'limits': {'dim0': (0.5, 2), 'dim2a': (10, 29)}},
+        {'name': 'ROI1', 'limits': {'dim0': (0, 0), 'dim2a': (25, 30)}},
+        {'name': 'ROI2', 'limits': {'dim0': (-1, 3), 'dim2a': (10, 20)}},
+        ]
+
+# Test 2: reduce (dim1, dim2)
+ROI_coords[1] = ['dim1', 'dim2']
+ROI_descs[1] = [
+        {'name': 'ROI0', 'limits': {'dim1': (0, 1), 'dim2': (1, 2.9)}},
+        {'name': 'ROI1', 'limits': {'dim1': (1, 1), 'dim2': (2.5, 3)}},
+        ]
+
+# Test 3: reduce dim0
+ROI_coords[2] = ['dim0']
+ROI_descs[2] = [
+        {'name': 'ROI0', 'limits': {'dim0': (0, 1)}},
+        {'name': 'ROI1', 'limits': {'dim0': (1, 2)}},
+        {'name': 'ROI2', 'limits': {'dim0': (0, 2)}},
+        {'name': 'ROI3', 'limits': {'dim0': (2, 2)}},
+        ]
+
+# Test 4: reduce all dims
+ROI_coords[3] = ['dim0', 'dim1', 'dim2']
+ROI_descs[3] = [
+        {'name': 'ROI0', 
+         'limits': {'dim0': (1, 2), 'dim1': (0, 1), 'dim2': (2, 3)}},
+        {'name': 'ROI1', 
+         'limits': {'dim0': (0, 2), 'dim1': (0, 0), 'dim2': (0, 1)}},
+        ]
+
+# Perform tests
+for test_num in range(4):
+    
+    # For the string array, combine ROI elements by string concatenation
+    reduce_fun = lambda Z, dims: reduce_fun_xarray(Z, dims,
+                                                   reduce_fun_1d_strjoin)
+    Ys = dpc.calc_xarray_ROIs(Xs, 'ROI', ROI_coords[test_num],
+                              ROI_descs[test_num], reduce_fun)
+    
+    # For the integer array, combine ROI elements by summation
+    reduce_fun = lambda Z, dims: Z.sum(dim=dims)
+    Y = dpc.calc_xarray_ROIs(X, 'ROI', ROI_coords[test_num],
+                             ROI_descs[test_num], reduce_fun)
+
+    print(f'======= TEST {test_num} =======\n\n')
+    print(Xs)
+    print('\n')
+    print(Ys)
+    print('\n')
+    print(Y)
+    print('\n')
+
+

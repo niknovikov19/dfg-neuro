573fe4fc5a6d2745cd706763c02dc639e0c5a6a4
niknovikov19 <nikknovikov@gmail.com>
2024-04-19 15:34:05 -0400

    Modify init file

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from .data_file_group_2 import DataFileGroup
+from .collapse import mean
 from . import data_file_group_2
+from . import useful as usf
 from . import TF
 from . import tf_power as tfpow
